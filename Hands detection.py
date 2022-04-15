3468792c49a866e8fade7a943c8e98fc2e6dabd6
MakarToporkov <94746654+MakarToporkov@users.noreply.github.com>
2022-04-15 17:29:26 +0300

    Add files via upload

--- Hands detection.py
+++ Hands detection.py
@@ -1,77 +1,57 @@
+import numpy as np
 import cv2
 import mediapipe as mp
 import time
+from PIL import Image
 import matplotlib.pyplot as plt
+import os
+from pynput.keyboard import Key, Listener
 
-import cv2
+p = os.path.abspath('file.txt ')
+
+
 
 cap = cv2.VideoCapture(0)
 
-'''while(1): 
-    ret, frame = cap.read()
-    cv2.imshow('video',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-
-cap.release()
-cv2.destroyAllWindows()'''
-
-cap = cv2.VideoCapture("res.avi")
-ret, frame = cap.read()
-while(1):
-   ret, frame = cap.read()
-   cv2.imshow('frame',frame)
-   if cv2.waitKey(1) & 0xFF == ord('q') or ret==False :
-       cap.release()
-       cv2.destroyAllWindows()
-       break
-
-   cv2.imshow('frame',frame)
-
-
-
-
-
-
-'''mpHands = mp.solutions.hands
+mpHands = mp.solutions.hands
 hands = mpHands.Hands()
-
 mpDraw = mp.solutions.drawing_utils
 
+def make_dot_screen():
+    pass
+                        
 
 while True:
+
             success, img = cap.read()
             imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            results = hands.process(imgRGB)
-
+            results = hands.process(imgRGB) 
             if results.multi_hand_landmarks:
                 for handLms in results.multi_hand_landmarks:
-                    pointsX =[]#создание массива
-                    pointsY =[]#создание массива
+                    #pointsX =[]#создание массива
+                    #pointsY =[]#создание массива
                     for id, lm in enumerate(handLms.landmark):
                         h, w, c = img.shape
                         cx, cy = int(lm.x*w), int(lm.y*h)
-                        pointsX.append(cx)
-                        pointsY.append(cy)
+                        print(cx, cy)
+                        #     with open(p, 'a' ) as file:
+                        #         file.writelines(str(cx)) 
+                        #         print(str(cx))
 
-                    # minX = min(pointsX) - int((min(pointsX))*0.12)попытка отступов
-                    # minY = min(pointsY) - int((min(pointsY))*0.08)попытка отступов
-                    # maxX = max(pointsX) + int((max(pointsX))*0.03)попытка отступов
-                    # maxY = max(pointsY) + int((max(pointsY))*0.01)попытка отступов
-                    minX = min(pointsX) #создание переменной для нахождения минимального значения массива по оси Х
-                    minY = min(pointsY) #создание переменной для нахождения минимального значения массива по оси Y
-                    maxX = max(pointsX) #создание переменной для нахождения максимального значения массива по оси Х
-                    maxY = max(pointsY) #создание переменной для нахождения максимаоьного значения массива по оси Y
+                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+      
+            # def on_press(key):
+            #     print('{0} pressed'.format(key))
 
-                    cv2.rectangle(img, ( minX, minY), (maxX, maxY), (255, 0 ,0))#построение прямоугольника по минимальным и максимальным точкам
+            # def on_release(key):
+            #     print('{0} release'.format(key))
 
+            #     if key == Key.space:
+            #         make_dot_screen()
 
-                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)'''
-
-
-#cv2.imshow("Image", img)
-           # cv2.waitKey(1)
-
-
+            cv2.imshow("", img)
+            cv2.waitKey(1)
+            
+            # with Listener(on_press=on_press, on_release=on_release) as listener:
+            #     listener.join()           
 
